4cd537e85e7ee5c159e85754e682c500040e1e6c
Juho Jeong <aspectran@gmail.com>
2022-03-30 23:42:32 +0900

    Update

--- js/chat-client-public.js
+++ js/chat-client-public.js
@@ -1,4 +1,5 @@
 let chatClient;
+let wrongPasscodeCount = 0;
 
 $(function () {
     if (!checkSignedIn()) {
@@ -6,9 +7,77 @@ $(function () {
         $("#message, #form-send-message button").prop("disabled", true);
         return false;
     }
-    chatClient = new ChatClientCore(chatClientSettings);
-    if (!chatClient.init()) {
-        return;
+    if (chatClientSettings.secret) {
+        $("#public-chat-enter-passcode .cancel").on("click", function () {
+            location.href = "/";
+        });
+        $("#form-passcode").submit(function () {
+            verifyPasscode();
+            return false;
+        });
     }
-    chatClient.readyToType();
+    chatClient = new ChatClientCore(chatClientSettings);
+    if (chatClientSettings.secret && !chatClientSettings.talkerToken) {
+        openPasscodePopup();
+    } else {
+        if (chatClient.init()) {
+            chatClient.readyToType();
+        }
+    }
 });
+
+function openPasscodePopup() {
+    $("#public-chat-enter-passcode").foundation('open');
+    $("#form-passcode .form-error.incorrect-passcode").hide();
+    $("#form-passcode input[name=passcode]").val("").focus();
+}
+
+function closePasscodePopup() {
+    $("#public-chat-enter-passcode").foundation('close');
+}
+
+function verifyPasscode() {
+    let passcode = $("#form-passcode input[name=passcode]").val().trim();
+    if (!passcode || passcode.length !== 4) {
+        $("#form-passcode input[name=passcode]").val("").focus();
+        return false;
+    }
+    closePasscodePopup();
+    openWaitPopup(modalMessages.signingIn, function () {
+        location.reload();
+    }, 10000);
+    $.ajax({
+        url: "/chats/" + chatClientSettings.chatId + "/verify",
+        data: {
+            passcode: passcode
+        },
+        method: 'POST',
+        dataType: 'text',
+        success: function (token) {
+            setTimeout(function () {
+                closeWaitPopup();
+                if (!token) {
+                    wrongPasscodeCount++;
+                    if (wrongPasscodeCount > 3) {
+                        chatClient.gotoHome();
+                        return;
+                    }
+                    openPasscodePopup();
+                    $("#form-passcode .form-error.incorrect-passcode").show();
+                } else {
+                    chatClientSettings.talkerToken = token;
+                    if (chatClient.init()) {
+                        chatClient.readyToType();
+                    } else {
+                        chatClient.gotoHome();
+                    }
+                }
+            }, 500);
+        },
+        error: function (e) {
+            console.error(e);
+            closeWaitPopup();
+            chatClient.serviceUnavailable();
+        }
+    });
+}
